e29910b455a076f0b5ee74d36a541bacec413827
alejandrokk16 <guerrerosuarez11@gmail.com>
2023-02-23 11:18:48 +0100

    Cambios jugador inicio del juego v0.1.3

--- Ajedrez/src/Ajedz/Jugadores.java
+++ Ajedrez/src/Ajedz/Jugadores.java
@@ -1,4 +1,7 @@
 package Ajedz;
+
+import java.util.Scanner;
+
 /**
  * Clase donde se alojan las variables de los dos jugadores 
  * 
@@ -33,6 +36,41 @@ public class Jugadores {
 	    
 	    public String getJugador2() {
 	        return jugador2;
+	    }
+	    
+	    public void inicioJuego() {
+	    	Scanner teclado = new Scanner(System.in);
+
+			String colorBlanco;
+			String colorNegro;
+
+			// pedimos a los jugadores que pidan un color
+			System.out.println("================================================");
+			System.out.println("digite una de las opciones prederterminada: ");
+			System.out.println("1. Color Blanco");
+			System.out.println("2. Color Negro ");
+			System.out.println("================================================");
+
+			String jugador1;
+			String jugador2;
+			// damos la opcion de esoger al jugador que color quiere tomar.
+			System.out.println("el primer jugador digite el color que desea: ");
+			jugador1 = teclado.next();
+
+			System.out.println("el segundo jugador digite el color que desea: ");
+			jugador2 = teclado.next();
+
+			// creamos un if y un else para saber quien empieza primero
+			if (jugador1.equals("1")) {
+
+				System.out.println("Jugador 1 empieza primero ya que ha tomado el color blanco.");
+
+			}
+
+			else {
+
+				System.out.println("Jugador 2 empieza primero ya que ha tomado el color blanco.");
+			}
 	    }
 	    
 }
